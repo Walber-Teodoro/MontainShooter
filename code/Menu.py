40e319f21fea73a2bf9207531d6f4de5e742fd84
Walber Teodoro <walbersteodoro@hotmail.com>
2026-03-11 18:49:26 -0300

    Creating main lopp Connecting Menu and Game classes

--- code/Menu.py
+++ code/Menu.py
@@ -18,11 +18,11 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", COLOR_ORANGE, (WIN_WIDTH / 2, 70))
-            self.menu_text(50, "Shooter", COLOR_ORANGE, (WIN_WIDTH / 2, 120))
+            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2),70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], C_WHITE, (WIN_WIDTH / 2, 160 + 25 * i))
+                self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
 
             pygame.display.flip()
 
@@ -32,8 +32,8 @@ class Menu:
                     pygame.quit()  # Close Window
                     quit()  # end pygame
 
-    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple, ):
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple,):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
-        self.window.blit(source=self.surf, dest=self.rect)
+        self.window.blit(source=text_surf, dest=text_rect)
