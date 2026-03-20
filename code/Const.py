895c11ca0034bc8bd3e9792697c1372d803e61ec
Walber Teodoro <walbersteodoro@hotmail.com>
2026-03-20 14:40:46 -0300

    Creating Mediator Pattern. Enemies destruction.

--- code/Const.py
+++ code/Const.py
@@ -1,11 +1,11 @@
-#C
+# C
 import pygame
 
-COLOR_ORANGE = (255,128,0)
-COLOR_WHITE = (255,255,255)
-COLOR_YELLOW = (255,255,0)
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 0)
 
-#E
+# E
 EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0': 0,
@@ -15,24 +15,37 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
-    'Player1'  : 4,
-    'Player2' :  4,
-    'Enemy1'   : 2,
-    'Enemy2'   : 1,
+    'Player1': 4,
+    'Player2': 4,
+    'Enemy1': 2,
+    'Enemy2': 1,
+}
+
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1': 300,
+    'Player2': 300,
+    'Enemy1': 50,
+    'Enemy2': 60,
 }
 
 # P
-PLAYER_KEY_UP ={'Player1': pygame.K_UP,
-                'Player2': pygame.K_w}
-PLAYER_KEY_DOWN ={'Player1': pygame.K_DOWN,
-                  'Player2': pygame.K_s}
-PLAYER_KEY_LEFT ={'Player1': pygame.K_LEFT,
-                  'Player2': pygame.K_a}
-PLAYER_KEY_RIGHT ={'Player1': pygame.K_RIGHT,
-                   'Player2': pygame.K_d}
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
-
 
 # M
 
@@ -42,7 +55,6 @@ MENU_OPTION = ('NEW GAME 1P',
                'SCORE',
                'EXIT')
 
-
 # W
-WIN_WIDTH= 576
-WIN_HEIGHT= 324
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -10,6 +10,7 @@ from code.Player import Player
 
 class EntityFactory:
 
+
     @staticmethod
     def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
