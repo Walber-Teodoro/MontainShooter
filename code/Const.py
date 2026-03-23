04ee7ee67acaf04696a88bb0a27278dc90906b94
Walber Teodoro <walbersteodoro@hotmail.com>
2026-03-23 20:53:51 -0300

    Hit Box Collisions HUD Score

--- code/Const.py
+++ code/Const.py
@@ -1,12 +1,34 @@
 # C
 import pygame
 
-COLOR_ORANGE = (255, 128, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_YELLOW = (255, 255, 0)
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 0)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
+
+
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
+
+ENTITY_DAMAGE ={
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 1,
+    'Player1Shot': 250,
+    'Player2': 4,
+    'Player2Shot': 25,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 15,
+}
 
 ENTITY_SPEED = {
     'Level1Bg0': 0,
@@ -52,6 +74,24 @@ ENTITY_SHOOT_DELAY = {
     'Enemy2': 100,
 }
 
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 0,
+    'Player1Shot': 0,
+    'Player2': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy1Shot': 0,
+    'Enemy2': 125,
+    'Enemy2Shot': 0,
+}
+
 # M
 
 MENU_OPTION = ('NEW GAME 1P',
@@ -75,6 +115,7 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 
 
 #S
+
 
 SPAWN_TIME = 400
 
